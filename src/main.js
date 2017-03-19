c21646c405ceb32a0cc6983bfe15a5d0a06dcb51
xm94630 <xm94630@126.com>
2017-03-19 22:19:36 +0800

    初始化vue 项目

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+console.log('===>')
+console.log(App)
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+var data = App.data;
+console.log(data().msg)
